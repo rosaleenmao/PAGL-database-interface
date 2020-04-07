901dd2fdbcd8bf12b2cd5a31552123bd7a66760e
Rosaleen Xiong <rosaleen@Rosaleens-MacBook-Pro.local>
2020-04-07 14:47:02 -0500

    testing commit, added comments to database_methods

--- database_methods.py
+++ database_methods.py
@@ -1,27 +1,34 @@
+# ensures arguments are passed in correctly
 def check_arguments(cat):
     print(cat)
 
 
+# ensures that csv fields are formatted correctly
 def check_fields(cat):
     print(cat)
     exit("Bad")
 
 
+# insert a new module into the database
 def insert_module(cat):
     print(cat)
 
 
+# remove a module from the database
 def remove(cat):
     print(cat)
 
 
+# insert new data into an existing module in the database
 def insert_module_data(cat):
     print(cat)
 
 
+# remove data from an existing module in the database
 def remove_module_data(cat):
     print(cat)
 
 
+# show what is stored in an existing module in the database
 def show_module(cat):
     print(cat)
