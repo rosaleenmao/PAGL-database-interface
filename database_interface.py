b472fd2314ae6fec438cc1787aece1c68e3f0a59
rosaleenmao <rosaleenmao@yahoo.com>
2020-04-09 22:43:02 -0500

    changed database structure itself so that deleting a record which is keyed to by other records will also delete the child records, then implemented remove module and remove data

--- database_interface.py
+++ database_interface.py
@@ -1,54 +1,25 @@
-import csv
-
 import database_methods
-
-import mysql.connector
 
 import sys
 
 # check_arguments ensures that args are formatted correctly and stores the correct action in to-do
 todo = database_methods.check_arguments(sys.argv)
 
-# csv file name
-filename = "test.csv"
-
-# initializing the titles and rows list
-fields = []
-rows = []
-
-# reading csv file
-with open(filename, 'r') as csvfile:
-    # creating a csv reader object
-    csvreader = csv.reader(csvfile)
-
-    # extracting field names through first row
-    fields = next(csvreader)
-
-    # extracting each data row one by one
-    for row in csvreader:
-        rows.append(row)
-
-        # get total number of rows
-    print("Total no. of rows: %d" % (csvreader.line_num))
-
-# printing the field names
-print('Field names are:' + ', '.join(field for field in fields))
-
-database_methods.check_fields("Dog")
-
-# mydb = mysql.connector.connect(
-#     host="34.69.95.10",
-#     user="root",
-#     passwd="PAGLIndia",
-#     database="pagl_india"
-# )
-#
-# mycursor = mydb.cursor()
-#
-# sql = "INSERT INTO modules (module_title, module_desc) VALUES (%s, %s)"
-# val = ("Test", "Test")
-# mycursor.execute(sql, val)
-#
-# mydb.commit()
-#
-# print(mycursor.rowcount, "record inserted.")
+if todo == "help":
+    database_methods.help_message()
+elif todo == "show all":
+    database_methods.show_all()
+elif todo == "show module":
+    database_methods.show_module(sys.argv[2])
+elif todo == "add module":
+    database_methods.add_module(sys.argv[2])
+elif todo == "add data":
+    database_methods.add_data(sys.argv[2], sys.argv[3])
+elif todo == "remove module":
+    database_methods.remove_module(sys.argv[2])
+elif todo == "remove data":
+    database_methods.remove_data(sys.argv[2])
+elif todo == "update module":
+    database_methods.update_module(sys.argv[2], sys.argv[3])
+elif todo == "update data":
+    database_methods.update_data(sys.argv[2], sys.argv[3])
--- database_methods.py
+++ database_methods.py
@@ -1,3 +1,28 @@
+import csv
+
+import mysql.connector
+
+
+# parses a csv and returns the data : fields and rows
+def read(filename):
+    fields = []
+    rows = []
+
+    # reading csv file
+    with open(filename, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+
+    check_fields(fields)
+
+    return rows
 
 
 # ensures arguments are passed in correctly
@@ -5,40 +30,59 @@
 def check_arguments(argv):
     n = len(argv)
 
-    # meets conditions to display help message
-    if n == 2 and ((argv[1] == "help") or (argv[1] == "-h") or (argv[1] == "-help")):
-        return "help"
-
-    # check if user wants to show all modules
-    elif n == 2 and ((argv[1] == "show_all") or (argv[1] == "-sa")):
-        return "show all"
-
     # not help message and doesn't have right number of arguments
-    if n != 3:
+    if n < 2 or n > 4:
         exit("Incorrect number of inputs : %d. "
              "First include what you want to use, then the file or title your module" % (n - 1))
 
+    # meets conditions to display help message
+    if n == 2:
+        if (argv[1] == "help") or (argv[1] == "-h") or (argv[1] == "-help"):
+            return "help"
+
+        # check if user wants to show all modules
+        elif (argv[1] == "show_all") or (argv[1] == "-sa"):
+            return "show all"
+
     # check if user wants to add module
-    if (argv[1] == "add_module") or (argv[1] == "-am"):
-        check_csv(argv[2])
-        return "add module"
+    if n == 3:
+        if (argv[1] == "add_module") or (argv[1] == "-am"):
+            check_csv(argv[2])
+            return "add module"
 
-    # check if user wants to add data
-    elif (argv[1] == "add_data") or (argv[1] == "-ad"):
-        check_csv(argv[2])
-        return "add data"
+        # check if user wants to remove module
+        elif (argv[1] == "remove_module") or (argv[1] == "-rm"):
+            check_int(argv[2])
+            return "remove module"
 
-    # check if user wants to remove module
-    elif (argv[1] == "remove_module") or (argv[1] == "-rm"):
-        return "remove module"
+        # check if user wants to remove data
+        elif (argv[1] == "remove_data") or (argv[1] == "-rd"):
+            check_int(argv[2])
+            return "remove data"
 
-    # check if user wants to remove data
-    elif (argv[1] == "remove_data") or (argv[1] == "-rd"):
-        return "remove data"
+        # check if user wants to show a specific module
+        elif (argv[1] == "show_module") or (argv[1] == "-sm"):
+            check_int(argv[2])
+            return "show module"
 
-    # check if user wants to show a specific module
-    elif (argv[1] == "show_module") or (argv[1] == "-sm"):
-        return "show module"
+    if n == 4:
+        # check if user wants to add data
+        if (argv[1] == "add_data") or (argv[1] == "-ad"):
+            check_int(argv[2])
+            check_csv(argv[3])
+            return "add data"
+
+        # check if user wants to add data
+        elif (argv[1] == "update_module") or (argv[1] == "-um"):
+            check_int(argv[2])
+            check_csv(argv[3])
+            return "update module"
+
+        # check if user wants to add data
+        if (argv[1] == "update_data") or (argv[1] == "-ud"):
+            check_int(argv[2])
+            check_csv(argv[3])
+            return "update data"
 
     # at this point, we know the user hasn't entered a valid command
     else:
@@ -49,7 +93,13 @@ def check_arguments(argv):
 # ensures that argument is csv
 def check_csv(csv_file):
     if csv_file[-4:] != ".csv":
-        exit("To add data or a module, you have to pass in a csv file.")
+        exit("To add data or a module, you have to pass in a csv file")
+
+
+# ensures that argument is an int
+def check_int(int_to_check):
+    if int_to_check.isdigit() is False:
+        exit("To remove or view a module or its data, you have to pass in its id")
 
 
 # prints help message in console
@@ -59,36 +109,89 @@ def help_message():
 
 
 # ensures that csv fields are formatted correctly
-# TODO
-def check_fields(cat):
-    print()
+def check_fields(fields):
+    if fields[0] != "type" or fields[1] != "content" or fields[2] != "text" or len(fields) != 3:
+        exit("Your spreadsheet must contain three fields: type, context, and text")
 
 
 # insert a new module into the database
 # TODO
-def insert_module(cat):
-    print(cat)
-
-
-# remove a module from the database
-# TODO
-def remove(cat):
-    print(cat)
+def add_module(cat):
+    print("am")
 
 
 # insert new data into an existing module in the database
 # TODO
-def insert_module_data(cat):
-    print(cat)
+def add_data(module_id, filename):
+    # initializing the titles and rows list
+    rows = []
+
+    rows = read(filename)
+
+    print("\n")
+    print(rows)
+    print("\n")
+
+
+# remove a module from the database
+def remove_module(module_id):
+    mydb = mysql.connector.connect(
+        host="34.69.95.10",
+        user="root",
+        passwd="PAGLIndia",
+        database="pagl_india"
+    )
+
+    mycursor = mydb.cursor()
+
+    sql = "DELETE FROM modules WHERE module_id = %s"
+    val = (module_id,)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "module removed.")
 
 
 # remove data from an existing module in the database
-# TODO
-def remove_module_data(cat):
-    print(cat)
+def remove_data(data_id):
+    mydb = mysql.connector.connect(
+        host="34.69.95.10",
+        user="root",
+        passwd="PAGLIndia",
+        database="pagl_india"
+    )
+
+    mycursor = mydb.cursor()
+
+    sql = "DELETE FROM module_data WHERE data_id = %s"
+    val = (data_id,)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, " data removed.")
 
 
 # show what is stored in an existing module in the database
 # TODO
 def show_module(cat):
-    print(cat)
+    print("sm")
+
+
+# show all the modules and their related metadata
+# TODO
+def show_all():
+    print("sa")
+
+
+# update module with given input
+# TODO
+def update_module(module_id, filename):
+    print("um")
+
+
+# update data with given input
+# TODO
+def update_data(data_id, filename):
+    print("ud")
