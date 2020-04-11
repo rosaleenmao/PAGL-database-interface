afd7d74b2cfad204ad6a4c63f7332599235e992f
rosaleenmao <rosaleenmao@yahoo.com>
2020-04-10 21:48:18 -0500

    wrote show all modules, show 1 module, insert data, and update data

--- database_methods.py
+++ database_methods.py
@@ -1,28 +1,7 @@
 import csv
 
 import mysql.connector
-
-
-# parses a csv and returns the data : fields and rows
-def read(filename):
-    fields = []
-    rows = []
-
-    # reading csv file
-    with open(filename, 'r') as csvfile:
-        # creating a csv reader object
-        csvreader = csv.reader(csvfile)
-
-        # extracting field names through first row
-        fields = next(csvreader)
-
-        # extracting each data row one by one
-        for row in csvreader:
-            rows.append(row)
-
-    check_fields(fields)
-
-    return rows
+# TODO - if module doesn't exist/can't fetch from database (update, remove, show) then throw an error
 
 
 # ensures arguments are passed in correctly
@@ -102,16 +81,38 @@ def check_int(int_to_check):
         exit("To remove or view a module or its data, you have to pass in its id")
 
 
-# prints help message in console
-# TODO
-def help_message():
-    print("dog")
+# parses a csv and returns the data : fields and rows
+def read(filename):
+    fields = []
+    rows = []
+
+    # reading csv file
+    with open(filename, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+
+    check_fields(fields)
+
+    return rows
 
 
 # ensures that csv fields are formatted correctly
 def check_fields(fields):
     if fields[0] != "type" or fields[1] != "content" or fields[2] != "text" or len(fields) != 3:
         exit("Your spreadsheet must contain three fields: type, context, and text")
+
+
+# prints help message in console
+# TODO
+def help_message():
+    print("dog")
 
 
 # insert a new module into the database
@@ -121,16 +122,30 @@ def add_module(cat):
 
 
 # insert new data into an existing module in the database
-# TODO
 def add_data(module_id, filename):
     # initializing the titles and rows list
-    rows = []
-
     rows = read(filename)
 
-    print("\n")
-    print(rows)
-    print("\n")
+    mydb = mysql.connector.connect(
+        host="34.69.95.10",
+        user="root",
+        passwd="PAGLIndia",
+        database="pagl_india"
+    )
+
+    mycursor = mydb.cursor()
+
+    data_inserted = 0
+
+    for row in rows:
+        sql = "INSERT INTO module_data (parent_module, content_type, content, text) values (%s, %s, %s, %s)"
+        val = (module_id, row[0], row[1], row[2])
+        mycursor.execute(sql, val)
+        data_inserted += mycursor.rowcount
+
+    mydb.commit()
+
+    print(data_inserted, "data inserted.")
 
 
 # remove a module from the database
@@ -174,24 +189,92 @@ def remove_data(data_id):
 
 
 # show what is stored in an existing module in the database
-# TODO
-def show_module(cat):
-    print("sm")
+def show_module(module_id):
+    mydb = mysql.connector.connect(
+        host="34.69.95.10",
+        user="root",
+        passwd="PAGLIndia",
+        database="pagl_india"
+    )
+
+    mycursor = mydb.cursor()
+
+    sql = "SELECT * FROM modules where module_id = %s"
+    val = (module_id,)
+    mycursor.execute(sql, val)
+
+    myresult = mycursor.fetchone()
+
+    print("Module %d:\n" % myresult[0])
+    print("   Module title: %s\n" % myresult[1])
+    print("   Module image: %s\n" % myresult[2])
+    print("   Module desc: %s\n\n" % myresult[3])
+
+    sql = "SELECT * FROM module_data where parent_module = %s"
+    val = (module_id,)
+    mycursor.execute(sql, val)
+
+    myresult = mycursor.fetchall()
+
+    for data in myresult:
+        print("Data %d:\n" % data[0])
+        print("   Data type: %s\n" % data[2])
+        print("   Data content: %s\n" % data[3])
+        print("   Data text: %s\n\n" % data[4])
 
 
 # show all the modules and their related metadata
-# TODO
 def show_all():
-    print("sa")
+    mydb = mysql.connector.connect(
+        host="34.69.95.10",
+        user="root",
+        passwd="PAGLIndia",
+        database="pagl_india"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM modules")
+
+    myresult = mycursor.fetchall()
+
+    for module in myresult:
+        print("Module %d:\n" % module[0])
+        print("   Module title: %s\n" % module[1])
+        print("   Module image: %s\n" % module[2])
+        print("   Module desc: %s\n\n" % module[3])
 
 
 # update module with given input
 # TODO
 def update_module(module_id, filename):
+    rows = read(filename)
     print("um")
 
 
 # update data with given input
-# TODO
 def update_data(data_id, filename):
-    print("ud")
+    # initializing the titles and rows list
+    rows = read(filename)
+
+    if len(rows) > 1:
+        exit("Can only update 1 piece of data at a time")
+
+    mydb = mysql.connector.connect(
+        host="34.69.95.10",
+        user="root",
+        passwd="PAGLIndia",
+        database="pagl_india"
+    )
+
+    mycursor = mydb.cursor()
+
+    row = rows[0]
+
+    sql = "UPDATE module_data SET content_type = %s, content = %s, text = %s WHERE data_id = %s"
+    val = (row[0], row[1], row[2], data_id)
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+
+    print(mycursor.rowcount, "data updated.")
